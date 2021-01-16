352eae0a955d079edf36f418b703cdb9334ada19
James Richardson <james+teal@time4tea.net>
2021-01-16 21:45:15 +0000

    - attempt to publish new version

--- release-ci.sh
+++ release-ci.sh
@@ -12,7 +12,7 @@ ensure_master
 BINTRAY_VERSION=`curl -s https://bintray.com/api/v1/packages/time4tea/oss/oidnjni/versions/_latest | jq -r .name`
 
 if [[ "$LOCAL_VERSION" == "$BINTRAY_VERSION" ]]; then
-    echo "Version has not changed"
+    echo "Version has not changed (local = $LOCAL_VERSION bintray = $BINTRAY_VERSION)"
     exit 0
 fi
 
