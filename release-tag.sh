f236eca91a1c200de69f01253d8f823e1c02e3ca
James Richardson <james+teal@time4tea.net>
2021-01-16 22:44:49 +0000

    - try alternative tag mechanism

--- release-tag.sh
+++ release-tag.sh
@@ -10,6 +10,30 @@ set -o nounset
 ensure_release_commit
 ensure_master
 
-git clone https://"${GITHUB_TOKEN}"@github.com/time4tea/oidnjni.git tmp/
+# from  https://github.com/anothrNick/github-tag-action/blob/master/entrypoint.sh  - MIT License
 
-( cd tmp && git config user.email "nobody@example.com" && git config user.name "CI Runner" && create_tag )
+commit=$(git rev-parse HEAD)
+new=$LOCAL_VERSION
+
+full_name=$GITHUB_REPOSITORY
+git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
+
+echo "$dt: **pushing tag $new to repo $full_name"
+
+git_refs_response=$(curl -s -X POST $git_refs_url -H "Authorization: token $GITHUB_TOKEN" -d @- << EOF
+{
+  "ref": "refs/tags/$new",
+  "sha": "$commit"
+}
+EOF
+)
+
+git_ref_posted=$( echo "${git_refs_response}" | jq .ref | tr -d '"' )
+
+echo "::debug::${git_refs_response}"
+if [ "${git_ref_posted}" = "refs/tags/${new}" ]; then
+  exit 0
+else
+  echo "::error::Tag was not created properly."
+  exit 1
+fi
