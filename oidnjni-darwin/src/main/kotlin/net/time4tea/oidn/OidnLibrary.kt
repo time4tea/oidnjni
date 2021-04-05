16f02eeb8036102e03bff752b3cfd8b1ff759d58
James Richardson <james@time4tea.net>
2021-04-05 21:18:09 +0100

    make macos version compile and run

--- oidnjni-darwin/src/main/kotlin/net/time4tea/oidn/OidnLibrary.kt
+++ oidnjni-darwin/src/main/kotlin/net/time4tea/oidn/OidnLibrary.kt
@@ -3,9 +3,9 @@ package net.time4tea.oidn
 data class Library(val name: String, val version: String? = null) {
     fun filename(): String {
         return if (version == null) {
-            "lib$name.so"
+            "lib$name.dylib"
         } else {
-            "lib$name.so.$version"
+            "lib$name.$version.dylib"
         }
     }
 }
@@ -13,8 +13,8 @@ data class Library(val name: String, val version: String? = null) {
 class OidnLibrary {
     companion object {
         val libraries = listOf(
-            Library("tbb", "2"),
-            Library("tbbmalloc", "2"),
+            Library("tbb"),
+            Library("tbbmalloc"),
             Library("OpenImageDenoise", "1.2.4"),
             Library("oidnjni")
         )
